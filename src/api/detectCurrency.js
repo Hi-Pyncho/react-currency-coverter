fdcaabe80d54a68e3ad6f500ceb32423b06fa9c8
Anton Mikaskin <garrakombo@gmail.com>
2022-09-24 14:41:11 +0300

    ADD: detect currency method

---
+++ src/api/detectCurrency.js
@@ -0,0 +1,20 @@
+import { getIp } from "./getIp"
+
+const detectCurrency = async () => {
+  const apiKey = '6x9dxiyw5o3gk16o'
+  const apiUrl = 'https://api.ipregistry.co/'
+  const ip = await getIp()
+  const requestUrl = new URL(ip, apiUrl)
+  requestUrl.searchParams.append('key', apiKey)
+  requestUrl.searchParams.append('fields', 'currency')
+
+  const response = await fetch(requestUrl).catch(err => {
+    throw new Error(err)
+  })
+
+  const data = await response.json()
+
+  return data.currency.code
+}
+
+export { detectCurrency }
