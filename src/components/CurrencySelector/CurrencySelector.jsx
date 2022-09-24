1fc74c2fdf1151d8cb1b07613da6394a904addba
Anton Mikaskin <garrakombo@gmail.com>
2022-09-24 13:42:13 +0300

    ADD: currency selector component

---
+++ src/components/CurrencySelector/CurrencySelector.jsx
@@ -0,0 +1,11 @@
+const CurrencySelector = ({ currencyList, currentCurrency, onChangeCurrency }) => {
+  return (
+    <select value={ currentCurrency } onChange={ onChangeCurrency }>
+      {
+        currencyList.map(item => <option key={ item }>{ item }</option>)
+      }
+    </select>
+  )
+}
+
+export { CurrencySelector }
