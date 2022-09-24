3da863852d25929c7b68bc2273b6e4dd22c7951f
Anton Mikaskin <garrakombo@gmail.com>
2022-09-24 19:01:31 +0300

    ADD: currency convert function

--- src/api/Currency.js
+++ src/api/Currency.js
@@ -1,13 +1,14 @@
 const apiPath = 'https://api.apilayer.com/exchangerates_data/'
+const headers = {
+  'apikey': 'D5psqVKgE4Dqhame9QlOammfOtwEa7Bo'
+}
 
 async function getCurrencySymbols() {
   const requestUrl = new URL('symbols', apiPath)
   const response = await fetch(requestUrl, {
     method: 'GET',
     redirect: 'follow',
-    headers: {
-      'apikey': 'D5psqVKgE4Dqhame9QlOammfOtwEa7Bo'
-    }
+    headers
   }).catch(err => {
     throw new Error(err)
   })
@@ -21,4 +22,23 @@ async function getCurrencySymbols() {
   }
 }
 
-export { getCurrencySymbols }
+async function convertCurrency(from, to, amount) {
+  const requestUrl = new URL('convert', apiPath)
+  requestUrl.searchParams.append('from', from)
+  requestUrl.searchParams.append('to', to)
+  requestUrl.searchParams.append('amount', amount)
+
+  const response = await fetch(requestUrl, {
+    method: 'GET',
+    redirect: 'follow',
+    headers
+  }).catch(err => {
+    throw new Error(err)
+  })
+
+  const rawAnswer = await response.json()
+  
+  return rawAnswer.result
+}
+
+export { getCurrencySymbols, convertCurrency }
