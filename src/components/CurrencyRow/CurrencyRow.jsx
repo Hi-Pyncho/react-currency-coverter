fc92a98fb9da869fc725522e3a40e948645ee1fa
Anton Mikaskin <garrakombo@gmail.com>
2022-09-25 20:04:10 +0300

    ADD: full width property

--- src/components/CurrencyRow/CurrencyRow.jsx
+++ src/components/CurrencyRow/CurrencyRow.jsx
@@ -3,6 +3,7 @@ import TextField from '@material-ui/core/TextField';
 const CurrencyRow = ({ currency, onChangeCurrencyInput }) => {
   return (
       <TextField 
+        fullWidth
         onChange={ onChangeCurrencyInput } 
         type='number' value={ currency } 
         id="outlined-basic" 
