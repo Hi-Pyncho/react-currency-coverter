f99678f9ffccc7d8c9c74669d0b083d469ab383c
Anton Mikaskin <garrakombo@gmail.com>
2022-09-26 12:22:48 +0300

    CHANGE: get currency api

--- src/api/Currency.js
+++ src/api/Currency.js
@@ -1,44 +1,11 @@
-const apiPath = 'https://api.apilayer.com/exchangerates_data/'
-const headers = {
-  'apikey': 'D5psqVKgE4Dqhame9QlOammfOtwEa7Bo'
-}
-
-async function getCurrencySymbols() {
-  const requestUrl = new URL('symbols', apiPath)
-  const response = await fetch(requestUrl, {
-    method: 'GET',
-    redirect: 'follow',
-    headers
-  }).catch(err => {
-    throw new Error(err)
-  })
-
-  const rawAnswer = await response.json()
-  const symbolsList = Object.keys(rawAnswer.symbols)
-
-  return {
-    raw: rawAnswer,
-    symbols: symbolsList
-  }
-}
-
 async function convertCurrency(from, to, amount) {
-  const requestUrl = new URL('convert', apiPath)
-  requestUrl.searchParams.append('from', from)
-  requestUrl.searchParams.append('to', to)
-  requestUrl.searchParams.append('amount', amount)
+  const apiPath = 'https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/'
+  const requestUrl = new URL(`${from}/${to}.json`, apiPath)
+ 
+  const response = await fetch(requestUrl)
+  const result = await response.json()
 
-  const response = await fetch(requestUrl, {
-    method: 'GET',
-    redirect: 'follow',
-    headers
-  }).catch(err => {
-    throw new Error(err)
-  })
-
-  const rawAnswer = await response.json()
-  
-  return rawAnswer.result
+  return (result[to] * amount).toFixed(2)
 }
 
-export { getCurrencySymbols, convertCurrency }
+export { convertCurrency }
